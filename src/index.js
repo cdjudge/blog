610400890c744b7622ab89b19e41dea5182c1170
Chris Judge <cdjiphone@gmail.com>
2019-02-27 18:08:06 -0500

    xxx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import App from "./components/App";
-import { createStore } from "./reducers";
+import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
@@ -12,5 +12,3 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
-
-export default index;
