99cdff0b654191d7139646e4ffc4fd441958701a
Chris Judge <cdjiphone@gmail.com>
2019-03-05 16:35:37 -0500

    xx

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,6 +1,6 @@
 import { bindActionCreators } from '../../../../../AppData/Local/Microsoft/TypeScript/3.3/node_modules/redux';
 
-export default (state = [], sction) => {
+export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS':
       return action.payload;
