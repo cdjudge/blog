833762bd3bf9cc7404d2d2dfcd64d1fb37358c5d
Chris Judge <cdjiphone@gmail.com>
2019-03-09 14:36:23 -0500

    Fixed some errors

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -14,6 +14,6 @@ const mapStateToProps = state => {
   return { users: state.users };
 };
 export default connect(
-  mapStateToUsers,
+  mapStateToProps,
   { fetchUser }
 )(UserHeader);
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,6 +1,8 @@
-export default (state, action) => {
+export default (state =[], action) => {
   switch (action.type) {
     case 'FETCH_USER':
       return [...state, action.payload];
+    default:
+      return state;
   }
 };
