ac3b725514bae5a7bf3a0d56200b69218f6bbeb7
cdjiphone@outlook.com <chrisjudge@Chriss-Air.hsd1.fl.comcast.net>
2019-03-09 15:21:18 -0500

    fixed some more issues

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,5 +1,3 @@
-import { bindActionCreators } from '../../../../../AppData/Local/Microsoft/TypeScript/3.3/node_modules/redux';
-
 export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS':
