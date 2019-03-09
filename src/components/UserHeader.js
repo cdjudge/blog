d095ac4c8acfa0462d8f41e937bd20f6ead32d0d
Chris Judge <cdjiphone@gmail.com>
2019-03-09 13:48:26 -0500

    x

---
+++ src/components/UserHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    fetchUser(this.props.userId);
+  }
+  render() {
+    return <div>User Header</div>;
+  }
+}
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+export default connect(
+  mapStateToUsers,
+  { fetchUser }
+)(UserHeader);
