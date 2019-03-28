9d10cbf1a4d4d9603166caf0697b4c19fe35f906
cdjiphone@outlook.com <chrisjudge@Chriss-Air.hsd1.fl.comcast.net>
2019-03-28 15:35:33 -0400

    stuff

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 import UserHeader from './UserHeader';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserAlt} from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUserAlt)
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faAngry } from '@fortawesome/free-regular-svg-icons';
+import {
+  List,
+  ListItemText,
+  ListItem,
+  Typography
+} from '@material-ui/core';
 
 class PostList extends Component {
   componentDidMount() {
@@ -16,21 +19,23 @@ class PostList extends Component {
   renderList() {
     return this.props.posts.map(post => {
       return (
-        <div className="item" key={post.id}>
-          <FontAwesomeIcon icon={faUserAlt}  size="2x"/>
-          <div className="content">
-            <div className="description">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-            </div>
-            <UserHeader userId={post.userId}/>
-          </div>
-        </div>
+        <ListItem key={post.id}>
+          <ListItemText
+            primary={
+              <React.Fragment>
+                  <FontAwesomeIcon icon={faAngry} size="2x" />
+                <UserHeader userId={post.userId} />
+                <Typography component="h2">{post.title}</Typography>
+              </React.Fragment>
+            }
+            secondary={post.body}
+          />
+        </ListItem>
       );
     });
   }
   render() {
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    return <List>{this.renderList()}</List>;
   }
 }
 const mapStateToProps = state => {
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -5,13 +5,14 @@ import { fetchUser } from '../actions';
 class UserHeader extends React.Component {
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
+    console.log(`user ${this.props.userId}`);
   }
   render() {
     const user = this.props.users.find(user => user.id === this.props.userId);
     if (!user) {
       return null;
     }
-    return <div className="header">{user.name}</div>;
+    return <span>{user.name}</span>;
   }
 }
 const mapStateToProps = state => {
